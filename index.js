923b5378ef5057f2c914db4c8883746e4a243b59
Janne Tolppanen <janne.ks@gmail.com>
2023-05-17 22:49:37 +0300

    4.2 done

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-
 const app = require('./app') // varsinainen Express-sovellus
 const logger = require('./utils/logger')
 const config = require('./utils/config')
-
-
-
-
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
---
+++ utils/middleware.js
@@ -0,0 +1,1 @@
+// add morgan and stuff idk
