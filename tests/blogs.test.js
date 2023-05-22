3b73b32f13767af981ba343abe67e4354c962272
Janne Tolppanen <janne.ks@gmail.com>
2023-05-22 20:24:48 +0300

    made tests to delete all testblogs and re-send them each time

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -2,32 +2,56 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
+const Blog = require('../models/blog')
 
-describe('.get /api/blogs', () => { 
+const initialBlogs = [
+  {
+    "title": "Gardening Wonders: The Ultimate Guide",
+    "author": "Emma GreenThumb",
+    "url": "https://www.gardensense.com",
+    "likes": 7
+  },
+  {
+    "title": "Tech Innovations: What's Next in AI?",
+    "author": "Martin Byte",
+    "url": "https://www.byteintofuture.com",
+    "likes": 5
+  },
+]
 
-test('blogs are returned as json', async () => {
-  const response = await api
-    .get('/api/blogs')
-
-  expect(response.header['content-type']).toContain('application/json')
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
 })
 
-test('length is 2', async () => {
-  const response = await api
-    .get('/api/blogs')    
+describe('.get /api/blogs', () => {
 
-  expect(response.body).toHaveLength(2)
-})
+  test('blogs are returned as json', async () => {
+    const response = await api
+      .get('/api/blogs')
 
-test.only('id exists', async () => {
-  const response = await api
-    .get('/api/blogs')
+    expect(response.header['content-type']).toContain('application/json')
+  })
+
+  test('length is 2', async () => {
+    const response = await api
+      .get('/api/blogs')
+
+    expect(response.body).toHaveLength(2)
+  })
+
+  test('id exists', async () => {
+    const response = await api
+      .get('/api/blogs')
 
     response.body.map(blog => {
       expect(blog.id).toBeDefined()
     })
 
-})
+  })
 
 })
 
