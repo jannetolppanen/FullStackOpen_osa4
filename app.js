8283020bf5cd276f799e3986f4b2042efc7d223f
Janne Tolppanen <janne.ks@gmail.com>
2023-05-27 20:28:22 +0300

    4.15 done. .post and .get to api/users works

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 mongoose.connect(config.MONGODB_URI)
 
@@ -12,5 +13,6 @@ app.use(middleware.requestLogger)
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
